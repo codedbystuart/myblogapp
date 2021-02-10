ec7dec79d765860cec96d94b93e5c9935eff99ba
codedbystuart <code.stuartdambi@gmail.com>
2021-02-10 13:24:51 +0300

    [add]: hash user passwords and generate jwt token

--- controllers/useController.js
+++ controllers/useController.js
@@ -1,16 +1,11 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const User = require('../models/UserModel');
 
 const UserController = {
   registerUser: async (req, res) => {
-    const user = new User({
-      _id: mongoose.Types.ObjectId(),
-      firstname: req.body.firstname,
-      lastname: req.body.lastname,
-      email: req.body.email,
-      password: req.body.password
-    });
-
     const userExists = await User.findOne({ email: req.body.email }).exec();
     if (userExists) {
       return res.status(409).json({
@@ -18,14 +13,35 @@ const UserController = {
         message: `uh oh, email ${userExists.email} has already been used`
       })
     }
+
+    const hashedPass = await bcrypt.hash(req.body.password, 10);
+
+    const user = new User({
+      _id: mongoose.Types.ObjectId(),
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      password: hashedPass
+    });
+
     user.save()
-      .then((result) => res.status(201).json({
-        status: res.statusCode,
-        result
-      }))
+      .then(async (result) => {
+        const user = {
+          id: result._id,
+          email: result.email,
+          firstName: result.firstName,
+          lastName: result.lastName
+        };
+        const token = await jwt.sign(user, process.env.SECRET_KEY);
+        return res.status(201).json({
+          status: res.statusCode,
+          result,
+          token
+        });
+      })
       .catch((error) => res.status(500).json({
         status: res.statusCode,
-        message: 'Ooops, something went wrong'
+        message: `Ooops, something went wrong: ${error}`
       }))
   }
 }
