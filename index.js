cd419c0a7703f82b0f0a8740b93739faf49fbe9c
codedbystuart <code.stuartdambi@gmail.com>
2021-02-10 13:25:32 +0300

    [add]: removed deprecated behaviour of mongoose

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
+const UserRouter = require('./routes/userRoutes');
 
 const app = express();
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
+app.use('/api', UserRouter);
 
-mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true});
 const db = mongoose.connection;
 db.on('error', () => console.error.bind('connection error:'));
 db.once('open', () => console.log('connected successfully'));
