f95c4ffaa253d2fc17d567b9e40fe957ba1420ab
codedbystuart <code.stuartdambi@gmail.com>
2021-02-10 13:56:32 +0300

    [add]: created blogs routes

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
 const UserRouter = require('./routes/userRoutes');
+const BlogRouter = require('./routes/blogRoutes');
 
 const app = express();
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', UserRouter);
+app.use('/posts', BlogRouter);
 
 mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true});
 const db = mongoose.connection;
