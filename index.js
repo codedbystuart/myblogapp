fa82a4393388b413bb0aba49f0fa0858aaa74a65
codedbystuart <code.stuartdambi@gmail.com>
2021-02-10 12:17:38 +0300

    [add]: added database connection

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true});
+const db = mongoose.connection;
+db.on('error', () => console.error.bind('connection error:'));
+db.once('open', () => console.log('connected successfully'));
 
 const PORT = process.env.PORT || 5000;
 
